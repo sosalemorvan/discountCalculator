8ee4479291a8decf30eaa2cb41e0d4baec62f0b3
sosalemorvan <67444245+sosalemorvan@users.noreply.github.com>
2025-03-12 23:22:53 +0000

    Arreglado

--- discount-calculator-pwa/server.js
+++ discount-calculator-pwa/server.js
@@ -2,13 +2,21 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Disable caching for development
+app.use((req, res, next) => {
+  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+  next();
+});
+
+// Serve static files
 app.use(express.static(path.join(__dirname)));
 
+// Route all requests to index.html
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
